ac16aebde1e9d835e4ce0a89af0dafe257446ee9
manoj victor <fmanojvictor@gmail.com>
2016-04-25 00:13:57 +0530

    materialise added

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,11 @@ gulp.task('default', function() {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./css/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./css/**/*.scss', ['sass']);
 });
